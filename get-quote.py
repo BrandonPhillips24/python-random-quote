7f4322c9b092b70c348a681bc7e8dbc44b7bf6c2
BrandonPhillips24 <brandonphillips2815@gmail.com>
2020-07-31 15:17:31 -0700

    Bug fixes
    Can't generate two of the same quotes now:)

--- get-quote.py
+++ get-quote.py
@@ -1,13 +1,21 @@
 import random
 rand = random.randint(0,19)
+rand2 = random.randint(0,19)
+while rand == rand2:
+  rand2 = random.randint(0,19)
+
+
 def primary():
  # print("Keep it logically awesome.")
   f = open("quotes.txt")
 
   quotes = f.readlines()
   f.close()
+  
+  myQuotes = "\n" + quotes[rand] + quotes[rand2]
+  print(myQuotes)
 
-  print(quotes[rand])
+  
 
 if __name__== "__main__":
   primary()
